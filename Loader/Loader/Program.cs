5baefd6c75c15aadf50513a26bdf9300541d3906
Мария <venkova_99@mail.ru>
2020-03-11 18:11:11 +0300

    changes3

--- Loader/Loader/Program.cs
+++ Loader/Loader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,92 +7,97 @@ namespace Loader
 {
     class Program
     {
-        // public static bool RunChallenge(Action<StreamReader, StreamWriter> challenge, string inputPath, string outputPath)
-        public static bool RunChallenge(int c, int d)
+        public static bool RunChallenge0(Action<StreamReader, StreamWriter> challenge, string SampleInput, string SampleOutput)
         {
             //check for file existence
-            string path = @"C:\Users\Даша\source\repos\tmp.txt";
-            if (File.Exists(path))
+            if (File.Exists(SampleInput)&& File.Exists(SampleOutput))
             {
-                string text1 ="";
-                string text2 ="";
-                //we consider the solution of the problem from the file
-                Console.WriteLine("Output:  ");
+                MemoryStream ms = new MemoryStream();
+                StreamWriter sw = new StreamWriter(ms);
+                string inputString, outputString, ourOutputString;
+
+                //read data from SampleInput
                 try
                 {
-                    using (StreamReader sr = new StreamReader(path))
+                    using (StreamReader sr = new StreamReader(SampleInput))
                     {
-                        Console.WriteLine(sr.ReadToEnd());
+                        inputString = sr.ReadToEnd();
+                        sr.BaseStream.Position = 0;
+                        challenge(sr, sw);
                     }
-                    using (StreamReader sr = new StreamReader(path))
+                    Console.WriteLine("Input String: ");
+                    Console.WriteLine(inputString); ;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    inputString = "";
+                }
+
+                //read data from SampleOutput
+                try
+                {
+                    using (StreamReader sr = new StreamReader(SampleOutput))
                     {
-                        text1 = sr.ReadToEnd();
+                        outputString = sr.ReadToEnd();
+                        Console.WriteLine("Output String: ");
+                        Console.WriteLine(outputString);
+                        sr.BaseStream.Position = 0;
+                        ms.Position = 0;
+                        using (StreamReader srr = new StreamReader(ms))
+                        {
+                            ourOutputString = srr.ReadToEnd();
+                            Console.WriteLine("Our Output String: ");
+                            Console.WriteLine(ourOutputString);
+                        }
                     }
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
+                    ourOutputString = "";
+                    outputString = "";
                 }
-
-                //entering our data into a file
-                Console.WriteLine("Program exit:  ");
-                string writePath = @"C:\Users\Даша\source\repos\tmp2.txt";
-                try
-                {
-                    using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
-                    {
-                        //sw.WriteLine("Дозапись");
-                        string C = Convert.ToString(c);
-                        string D = Convert.ToString(d);
-                        sw.Write(C);
-                        sw.Write(D);
-                    }
-                    using (StreamReader sr = new StreamReader(writePath))
-                    {
-                        Console.WriteLine(sr.ReadToEnd());
-                    }
-                    using (StreamReader sr = new StreamReader(writePath))
-                    {
-                        text2=sr.ReadToEnd();
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-
-                //compare of two lines
-                if (text1 == text2) {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-                /*
-                //cравнение двух файлов сначала по длине, потом по байтам
-                if (new FileInfo(path).Length != new FileInfo(writePath).Length)
-                {
-                    return false;
-                }
-                else
-                {
-                    return (File.ReadAllBytes(path).SequenceEqual(File.ReadAllBytes(writePath)));
-                }
-                */
+                return (outputString == ourOutputString);
             }
             else 
-            { 
+            {
                 return false;
             }
-            
         }
 
-
-        static void Main(string[] args)
+        public static int[] gradingStudents(int[] grades)
         {
-            //if (result==RunChellenge(Action < StreamReader, StreamWriter > challenge, inputPath, outputPath))
-            Console.WriteLine(RunChallenge(2, 3));
+            for (int i = 0; i < grades.Length; i++)
+            {
+                var item = grades[i];
+                if (item >= 38)
+                {
+                    var diff = 5 - (item % 5);
+                    if (diff < 3)
+                        grades[i] = item + diff;
+                }
+            }
+            return grades;
+        }
+        public static void challenge(StreamReader streamReader, StreamWriter streamWriter)
+        {
+            TextWriter textWriter = streamWriter;
+            int gradesCount = Convert.ToInt32(streamReader.ReadLine());
+            int[] grades = new int[gradesCount];
+            for (int i = 0; i < gradesCount; i++)
+            {
+                grades[i] = Convert.ToInt32(streamReader.ReadLine());
+            }
+            int[] result = gradingStudents(grades);
+            textWriter.WriteLine(String.Join("\n", result));
+            textWriter.Flush();    
+        }
+        public static void Main(string[] args)
+        {
+            string SampleInput = @"C:\Users\Даша\source\repos\tmp.txt";
+            string SampleOutput = @"C:\Users\Даша\source\repos\tmp2.txt";
+            Console.WriteLine(RunChallenge0(challenge, SampleInput, SampleOutput));
         }
     }
 }
