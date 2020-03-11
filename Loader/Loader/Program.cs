7c404e9fad32ae364ddbcdbaf493d75595150ebf
Мария <venkova_99@mail.ru>
2020-03-11 12:19:56 +0300

    changes 2

--- Loader/Loader/Program.cs
+++ Loader/Loader/Program.cs
@@ -6,70 +6,92 @@ namespace Loader
 {
     class Program
     {
-        // public static bool RunChellenge(Action<StreamReader, StreamWriter> challenge, string inputPath, string outputPath)
-        public static bool RunChellenge(int c, int d)
+        // public static bool RunChallenge(Action<StreamReader, StreamWriter> challenge, string inputPath, string outputPath)
+        public static bool RunChallenge(int c, int d)
         {
-            ////////////////////проверка существования
+            //check for file existence
             string path = @"C:\Users\Даша\source\repos\tmp.txt";
-            if (Directory.Exists(path))
+            if (File.Exists(path))
             {
-                Console.WriteLine("File is not found");
-            }
-
-            ///////////////////считаем весь текст из полностью записанного ранее файла
-            Console.WriteLine("Output:  ");
-            try
-            {
-                using (StreamReader sr = new StreamReader(path))
+                string text1 ="";
+                string text2 ="";
+                //we consider the solution of the problem from the file
+                Console.WriteLine("Output:  ");
+                try
                 {
-                    Console.WriteLine(sr.ReadToEnd());
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        text1 = sr.ReadToEnd();
+                    }
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            ////////////////////вывод строки или числа в пустой файл
-            Console.WriteLine("Program exit:  ");
-            string writePath = @"C:\Users\Даша\source\repos\tmp2.txt";
-            try
-            {
-                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                catch (Exception e)
                 {
-                    //sw.WriteLine("Дозапись");
-                    string C = Convert.ToString(c);
-                    string D = Convert.ToString(d);
-                    sw.Write(C);
-                    sw.Write(D);
+                    Console.WriteLine(e.Message);
                 }
-                using (StreamReader sr = new StreamReader(writePath))
-                {
-                    Console.WriteLine(sr.ReadToEnd());
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
 
-            //////////////////cравнение двух файлов сначала по длине, потом по байтам
-            if (new FileInfo(path).Length != new FileInfo(writePath).Length)
-            {
+                //entering our data into a file
+                Console.WriteLine("Program exit:  ");
+                string writePath = @"C:\Users\Даша\source\repos\tmp2.txt";
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                    {
+                        //sw.WriteLine("Дозапись");
+                        string C = Convert.ToString(c);
+                        string D = Convert.ToString(d);
+                        sw.Write(C);
+                        sw.Write(D);
+                    }
+                    using (StreamReader sr = new StreamReader(writePath))
+                    {
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                    using (StreamReader sr = new StreamReader(writePath))
+                    {
+                        text2=sr.ReadToEnd();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                //compare of two lines
+                if (text1 == text2) {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                /*
+                //cравнение двух файлов сначала по длине, потом по байтам
+                if (new FileInfo(path).Length != new FileInfo(writePath).Length)
+                {
+                    return false;
+                }
+                else
+                {
+                    return (File.ReadAllBytes(path).SequenceEqual(File.ReadAllBytes(writePath)));
+                }
+                */
+            }
+            else 
+            { 
                 return false;
             }
-            else
-            {
-                return (File.ReadAllBytes(path).SequenceEqual(File.ReadAllBytes(writePath)));
-            }
+            
         }
 
 
         static void Main(string[] args)
         {
             //if (result==RunChellenge(Action < StreamReader, StreamWriter > challenge, inputPath, outputPath))
-
-            Console.WriteLine(RunChellenge(2, 3));
+            Console.WriteLine(RunChallenge(2, 3));
         }
     }
 }
